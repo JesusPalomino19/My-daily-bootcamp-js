fdad466503fa3b39cea6e82913285f7ea96f9803
vladimir <vrodriguezalvarez4@gmail.com>
2022-07-12 18:19:00 -0500

    deleted semi-completed

--- main.js
+++ main.js
@@ -28,3 +28,14 @@
         },600)
     }
  })
+let menus = document.querySelectorAll(".popup-container .public-button");
+let content = document.querySelectorAll(".popup-container .popup");
+
+
+menus.forEach((element,index) => {
+    element.addEventListener("click", function (e) {
+        e.preventDefault();
+        content[index].style.opacity = "1";
+        content[index].style.display = "block";
+     });
+});
